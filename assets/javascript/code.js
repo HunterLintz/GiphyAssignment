b91ee9e269b6e3e1ee639d6cc2ea6269e7402472
Hunter Lintz <hunterlintz126@gmail.com>
2020-03-16 22:27:58 -0400

    Fixed Search to check if input is a pokemon so unwanted inputs arent set as buttons

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,4 +1,11 @@
 var pokemon = ["Pikachu","Ditto","Mew","Meowth","Lapras","Darkrai","Squirtle","Charizard"];
+var pokeCheck = [];
+var json = $.getJSON("https://raw.githubusercontent.com/sindresorhus/pokemon/master/data/en.json",function(data){
+	$.each(data,function(i,v){
+		pokeCheck.push(v.toLowerCase());
+	});
+	console.log(pokeCheck);
+});
 var currentPokemon;
 $( document).ready(function(){
 	for (i = 0; i < pokemon.length; i++){
@@ -9,7 +16,7 @@ $( document).ready(function(){
 		loadImages();
 	});
 	function loadImages(){
-		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + currentPokemon +"+pokemon&api_key=FEC4atVGyyKs8JkSHVUQyI0bto3q6rDC&limit=10";
+		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + currentPokemon +"+pokemon&rating=PG&=&api_key=FEC4atVGyyKs8JkSHVUQyI0bto3q6rDC&limit=10";
 		$("#gif-container").empty();
 		$.ajax({
 			url:queryURL,
@@ -22,7 +29,20 @@ $( document).ready(function(){
 			});
 	};
 	$("#submit").on("click",function(){
-		var poke = $("#addMonster").val()
-		$("#button-container").append("<button class = 'monsterButton' value = '"+ poke +"' type='button'>"+poke+"</button>")
+		var poke = $("#addMonster").val().toLowerCase();
+		poke = poke.replace(/\s/g, '');
+		console.log(poke);
+		var pokemane = $.inArray(poke,pokeCheck);
+		console.log(pokemane)
+		if (pokemane == -1){
+			$("#addMonster").val("");
+			console.log("Your suck at coding")
+		}else{
+			console.log(poke)
+			poke = poke.toString().substring(0, 1).toUpperCase() + poke.toString().substring(1);
+			console.log(poke)
+			console.log("YOur kinda cool")
+			$("#button-container").append("<button class = 'monsterButton' value = '"+ poke +"' type='button'>"+poke+"</button>");
+		}
 	});
 });
