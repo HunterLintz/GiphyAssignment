98d41f6c19553a0c04a701cf52d79ed49aa85ab5
Hunter Lintz <hunterlintz126@gmail.com>
2020-03-17 00:35:28 -0400

    Small Tweaks
    Added Favicon, Changed title, added comments to JS

--- assets/javascript/code.js
+++ assets/javascript/code.js
@@ -1,5 +1,8 @@
+//Array of all pokemon that are already buttons
 var pokemon = ["Pikachu","Ditto","Mew","Meowth","Lapras","Darkrai","Squirtle","Charizard"];
+//Array of all Pokemon
 var pokeCheck = [];
+//grabbing a json file of all pokemon for checking user input
 var json = $.getJSON("https://raw.githubusercontent.com/sindresorhus/pokemon/master/data/en.json",function(data){
 	$.each(data,function(i,v){
 		pokeCheck.push(v.toLowerCase());
@@ -8,6 +11,7 @@ var json = $.getJSON("https://raw.githubusercontent.com/sindresorhus/pokemon/mas
 });
 var currentPokemon;
 $( document).ready(function(){
+	//creates a button for every item in the pokemon array at the start of the program
 	for (i = 0; i < pokemon.length; i++){
 		$("#button-container").append("<button class ='monsterButton' value = '"+pokemon[i]+"' type='button'>"+pokemon[i]+"</button>")
 	}
@@ -15,6 +19,7 @@ $( document).ready(function(){
 		currentPokemon = $(this).val();
 		loadImages();
 	});
+	//function to load images on button click
 	function loadImages(){
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + currentPokemon +"+%20pokemon&rating=PG&=&api_key=FEC4atVGyyKs8JkSHVUQyI0bto3q6rDC&limit=10";
 		$("#gif-container").empty();
@@ -23,11 +28,13 @@ $( document).ready(function(){
 			method:"GET"
 		})
 			.then(function(response){
+				console.log(response);
 				for (v = 0; v < response.data.length;v++){
 					$("#gif-container").append("<span class = 'gifs'><img src='"+ response.data[v].images.original.url +"'><div class='rating'>Rating: "+response.data[v].rating+"</div></span>")
 				}
 			});
 	};
+	//function for submitting pokemon
 	$("#submit").on("click",function(){
 		var poke = $("#addMonster").val().toLowerCase();
 		poke = poke.replace(/\s/g, '');
@@ -50,17 +57,18 @@ $( document).ready(function(){
 		}
 		$("#addMonster").val("");
 	});
+	//functions for the wrong input alert animation
 	function testAlertON(){
 		$('#alert').css('opacity','1');
 		$('#alertText').css('opacity','1');
 		setTimeout(function(){testAlertOFF();},5000);
 		}
-		
 		function testAlertOFF(){
 		$('#alert').css('opacity','0');
 		$('#alertText').css('opacity','0');
 		}
 	});
+	//function to check if enter is pressed in input
 	$('#addMonster').keypress(function (e) {
 		if (e.which == 13) {
 			$('#submit').click()
